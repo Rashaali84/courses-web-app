fb6d4d5761c4783df4be79e786b036f7b8b4612a
Rasha ali <rashaali84@gmail.com>
2020-05-19 23:38:02 +0200

    client stages

---
+++ client/app/actions.js
@@ -0,0 +1,47 @@
+const saveCourse = (courseName) => {
+    fetch('/api/courses/', {
+        method: 'POST',
+        body: JSON.stringify({ name: courseName }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then(() => {
+            alert('your changes are saved');
+        })
+        .then(() => { location.reload(); })
+        .catch(err => {
+            console.error(err);
+            alert('unable to save your changes');
+        });
+}
+const updateCourse = (courseName, courseId) => {
+    fetch('/api/courses/' + courseId, {
+        method: 'PUT',
+        body: JSON.stringify({ name: courseName }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then(() => {
+            alert('your changes are updates');
+        })
+        .then(() => { location.reload(); })
+        .catch(err => {
+            console.error(err);
+            alert('unable to update your changes');
+        });
+}
+const deleteCourse = (courseId) => {
+    fetch('/api/courses/' + courseId, {
+        method: 'DELETE',
+    })
+        .then(() => {
+            alert('entry is deleted');
+        })
+        .then(() => { location.reload(); })
+        .catch(err => {
+            alert('unable to delete entry');
+            console.error(err);
+        });
+}
--- client/app/init.js
+++ client/app/init.js
@@ -1,5 +1,39 @@
-export const init = async () => {
-  const res = await fetch('/api');
-  const data = await res.json();
-  console.log(data);
-};
+export const init = () => {
+  fetch('/api/courses/', {
+    method: 'GET'
+  })
+    .then(res => {
+      if (!res.ok) {
+        throw res;
+      }
+      return res.json();
+    })
+    .then(records => {
+      const container = document.getElementById('container');
+      container.innerHTML = JSON.stringify(records);
+      if (records.length > 0) {
+        container.innerHTML = 'No entries yet stored in db..';
+      }
+    })
+    .catch(err => console.error(err));
+
+  document.getElementById('save-button')
+    .addEventListener('click', (e) => {
+      const courseName = document.getElementById('name').value;
+      saveCourse(courseName);
+      e.preventDefault();
+    });
+  document.getElementById('update-button')
+    .addEventListener('click', (e) => {
+      const courseName = document.getElementById('name').value;
+      const courseId = document.getElementById('courseId').value;
+      updateCourse(courseName, courseId);
+      e.preventDefault();
+    });
+  document.getElementById('delete-button')
+    .addEventListener('click', (e) => {
+      const courseId = document.getElementById('courseId').value;
+      deleteCourse(courseId);
+      e.preventDefault();
+    });
+}
